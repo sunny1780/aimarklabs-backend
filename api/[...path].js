aca35d64f1248b93d9b9e70c9a233f2fc0202078
farhan8765 <farhan@aimarklabs.com>
2026-03-13 12:00:47 +0500

    jkkk

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,28 @@
 const { requestHandler } = require('../server');
 
-module.exports = (req, res) => requestHandler(req, res);
+module.exports = (req, res) => {
+  const query = { ...(req.query || {}) };
+  const pathSegments = Array.isArray(query.path)
+    ? query.path
+    : query.path
+      ? [query.path]
+      : [];
+
+  delete query.path;
+
+  const search = new URLSearchParams();
+  Object.entries(query).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((item) => search.append(key, String(item)));
+      return;
+    }
+    if (value !== undefined) {
+      search.append(key, String(value));
+    }
+  });
+
+  const pathname = `/api/${pathSegments.join('/')}`;
+  req.url = search.size > 0 ? `${pathname}?${search.toString()}` : pathname;
+
+  return requestHandler(req, res);
+};
