1e23526041f10efe7da90be28b82d98333bbcfee
farhan8765 <farhan@aimarklabs.com>
2026-03-13 12:44:44 +0500

    jhsdjhjdf

---
+++ api/google-analytics/overview.js
@@ -0,0 +1,21 @@
+const { requestHandler } = require('../../server');
+
+module.exports = (req, res) => {
+  const query = new URLSearchParams();
+
+  Object.entries(req.query || {}).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((item) => query.append(key, String(item)));
+      return;
+    }
+    if (value !== undefined) {
+      query.append(key, String(value));
+    }
+  });
+
+  req.url = query.size > 0
+    ? `/api/google-analytics/overview?${query.toString()}`
+    : '/api/google-analytics/overview';
+
+  return requestHandler(req, res);
+};
